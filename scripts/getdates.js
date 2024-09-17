791d4d2513ff2cedae37ab69e3fa604e067d277a
mislangc <carlbpmislang@gmail.com>
2024-09-17 20:47:58 +0800

    4. Home page done. First commit

---
+++ scripts/getdates.js
@@ -0,0 +1,10 @@
+const year = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+// get the current year for footer
+const date = new Date();
+year.innerHTML = date.getFullYear();
+
+//get the last modification date of the home page
+let lastMod = document.lastModified;
+lastModified.innerHTML = `Last Modified: ${lastMod}`;
